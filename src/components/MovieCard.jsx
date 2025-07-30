5940fdebb0c0a7991a9bb170a5fc49cf9228ed26
rolindaz <rolinda.zamboni@gmail.com>
2025-07-30 11:13:05 +0200

    M4: Update index.html title and favicon; enhance MovieCard component with overview and improved styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,7 @@ export default function MovieCard({ movie }) {
     return (
         <div key={movie.id} className="col">
             <div className="card h-100 p-2">
-                <div className="card-top">
+                <div className="card-top h-100 top-1">
                     {
                         movie.poster_path ? (
                             <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title || movie.name} className="card-img-top" />
@@ -23,24 +23,24 @@ export default function MovieCard({ movie }) {
                         )
                     }
                 </div>
-                <div className="card-body">
-                    <ul className="list-unstyled">
-                        <li>
+                <div className="card-top top-2 p-3">
+                    <ul className="list-group list-group-flush justify-content-center">
+                        <li className="list-group-item">
                             <h3>
                                 {movie.title || movie.name}
                             </h3>
                         </li>
-                        <li>
+                        <li className="list-group-item">
                             <b>Original title: </b>
                             {movie.original_title || movie.original_name}
                         </li>
-                        <li className="language d-flex">
+                        <li className="list-group-item language d-flex">
                             <b>Original language: </b>
                             {
                                 <Flag code={movie.original_language} fallback={<span>{movie.original_language}</span>} />
                             }
                         </li>
-                        <li>
+                        <li className="list-group-item">
                             <b>Vote: </b>
                             <span>
                                 {
@@ -55,6 +55,13 @@ export default function MovieCard({ movie }) {
                                     })
                                 }
                                 {` (${Math.ceil((movie.vote_average * 5) / 10)} Stars)`}
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <b>Overview: </b>
+                            <br />
+                            <span>
+                                {movie.overview}
                             </span>
                         </li>
                     </ul>
